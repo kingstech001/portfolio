5aa2afd27e968248b8df9b74fc005a32c8341a3e
KINGSLEY <mamahkingsleychukwuebuka@gmail.com>
2024-06-11 02:28:50 +0100

    my last commit

---
+++ src/components/navLinks.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const NavLinks = () => {
+  return (
+    <>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "border-b-2 border-white font-bold" : "font-bold"
+          }
+          to={"/"}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "border-b-2 border-[white] font-bold " : "font-bold"
+          }
+          to={"about"}
+        >
+          About
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "border-b-2 border-[white ]font-bold " : "font-bold"
+          }
+          to={"services"}
+        >
+          Services
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "border-b-2 border-[white ]font-bold" : "font-bold"
+          }
+          to={"contact"}
+        >
+          Contact
+        </NavLink>
+      </>
+  )
+}
+
+export default NavLinks
